2f1eff1d079ad6c1ed4268bed708e19460b8ff13
Kayland Childress <113496012+Kayland-Childress@users.noreply.github.com>
2024-02-12 04:22:27 +0000

    put JS in its own file and linked it. Still can't figure out why it is not clearing all the way

--- circle/assets/js/circle.js
+++ circle/assets/js/circle.js
@@ -1,48 +1,56 @@
 // Circle JavaScrip
 
-$("#CircleForm").validate({});
-
-//Calculate button click event
-$("#btnSubmit").click(function () {
+function displayRadius() {
   if ($("#CircleForm").valid()) {
-    calculateResults();
+    document.getElementById("radius").innerHTML = "";
+
+    var radius; // string representation of the radius
+    var radiusfp; // floating point value of radius
+    var diameter; // floating point diameter
+    var circumference; // floating point circumference
+    var area; // floating point area
+    var result; // displayable result
+
+    // read in radius as a string
+    radius = document.getElementById("radius").value;
+
+    // convert radius from string to floating point
+    radiusfp = parseFloat(radius);
+
+    // calculate diameter, circumference, area
+    diameter = calcDiameter(radiusfp);
+    circumference = calcCircumference(radiusfp);
+    area = calcArea(radiusfp);
+
+    // display the hypotenuse
+    document.getElementById("diameter").innerHTML = diameter.toString();
+    document.getElementById("circumference").innerHTML =
+      circumference.toString();
+    document.getElementById("area").innerHTML = area.toString();
   }
-});
-
-//Clear button click event
-$("#btnClear").click(function () {
-  clearForm();
-});
-
-function calculateResults() {
-  //Get the Radius value
-  var radius = parseFloat($("#radius").val());
-
-  //Calculate diameter, circumfrence, and area
-  var diameter = calcDiameter(radius);
-  var circumference = calcCircumference(radius);
-  var area = calcArea(radius);
-
-  //Display results
-  $("#diameterResult").text(diameter.toFixed(2));
-  $("#circumferenceResult").text(circumference.toFixed(2));
-  $("#areaResult").text(area.toFixed(2));
 }
 
+// returns diameter of radius
 function calcDiameter(radius) {
   return 2 * radius;
 }
 
+//returns circumference of radius
 function calcCircumference(radius) {
   return 2 * Math.PI * radius;
 }
 
+//returns area of radius
 function calcArea(radius) {
-  return Math.PI * Math.pow(radius, 2);
+  return Math.PI * radius * radius;
 }
 
 function clearForm() {
-  //Reset form and results
-  $("#CircleForm")[0].reset();
-  $("#diameterResult, #circumferenceResult, #areaResult").text("");
+  document.getElementById("diameter").innerHTML = "";
+  document.getElementById("circumference").innerHTML = "";
+  document.getElementById("area").innerHTML = "";
+  document.getElementById("radius").value = "";
+  document.getElementById("radiuserror").value = "";
 }
+
+
